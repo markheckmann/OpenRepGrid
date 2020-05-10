13ca8807635c0892591f148910960495a217b728
Mark Heckmann <heckmann.mark@gmail.com>
2020-05-10 02:18:46 +0200

    test for indexDilemma

--- tests/testthat/test-indexes.R
+++ tests/testthat/test-indexes.R
@@ -1,7 +1,14 @@
 # Test for for indexes to return correct values, e.g.
 # tested against other software
 
+
 test_that("PVAFF returns expected value", {
   v <- indexPvaff(boeker)
   expect_equal(v, 0.42007)
+})
+
+
+test_that("indexDilemma matches Gridcor results", {
+  id <- indexDilemma(boeker, self = 1, ideal = 2, r.min = .35)
+  expect_equal(id$no_ids, 4)
 })
