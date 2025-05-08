b79e5a8d89eeb3c159ecc501d9b44d291dabadaf
Mark Heckmann <mark.heckmann@deutschebahn.com>
2025-05-08 20:48:23 +0200

    biplot3d: new args c.axis.show, c.sphere.show (#9)

---
+++ inst/examples/example-biplot3d.R
@@ -0,0 +1,13 @@
+\dontrun{
+biplot3d(boeker)
+
+biplot3d(boeker, e.sphere.show = 1:4)
+biplot3d(boeker, c.axis.show = 1:2)
+
+biplot3d(boeker, e.sphere.col = "red", c.text.col = "blue")
+biplot3d(boeker, e.cex = 1)
+biplot3d(boeker, col.sphere = "red")
+
+biplot3d(boeker, g = 1, h = 1) # INGRID biplot
+biplot3d(boeker, g = 1, h = 1, center = 4) # ESA biplot
+}
