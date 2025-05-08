54e053b01a7487d6f0d931e05a1cd1aad8c9cabb
Mark Heckmann <mark.heckmann@deutschebahn.com>
2025-05-08 20:51:02 +0200

    biplot3d: arg `e.labels.show`

--- inst/examples/example-biplot3d.R
+++ inst/examples/example-biplot3d.R
@@ -2,6 +2,7 @@
 biplot3d(boeker)
 
 biplot3d(boeker, e.sphere.show = 1:4)
+biplot3d(boeker, e.sphere.show = 1:4, e.labels.show = 1:2)
 biplot3d(boeker, c.axis.show = 1:2)
 
 biplot3d(boeker, e.sphere.col = "red", c.text.col = "blue")
