f69f6db1043982ef659d5ed913c880c032222b67
Mark Heckmann <mark.heckmann@deutschebahn.com>
2025-07-08 15:17:52 +0200

    run styler & pkgdown

--- R/utils.r
+++ R/utils.r
@@ -1044,8 +1044,9 @@ get_names_na <- function(x) {
 }
 
 # from tools::file_path_sans_ext
-file_path_sans_ext <- function (x, compression = FALSE) {
-  if (compression)
+file_path_sans_ext <- function(x, compression = FALSE) {
+  if (compression) {
     x <- sub("[.](gz|bz2|xz)$", "", x)
+  }
   sub("([^.]+)\\.[[:alnum:]]+$", "\\1", x)
 }
